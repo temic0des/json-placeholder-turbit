38167a5eb9293f9f0372610c22469cdb0004946c
Temi Rahman <Temi@Temi.fritz.box>
2024-07-17 16:02:43 +0200

    Comments added to the photo feature

--- app/routers/photos/photo_endpoint.py
+++ app/routers/photos/photo_endpoint.py
@@ -18,7 +18,16 @@ class PhotoEndpoint:
         self.photo_router.get('/{photo_id}', response_model=PhotoRead)(self.fetch_photo_by_id)
         self.photo_router.get('', response_model=List[PhotoRead])(self.fetch_limited_photos)
 
-    async def fetch_all_photos(self, photo_service: PhotoService = Depends(get_photo_service)):
+    async def fetch_all_photos(self, photo_service: PhotoService = Depends(get_photo_service)) -> List[PhotoRead]:
+        """
+            Fetches all the photos
+
+            Args:
+                photo_service (PhotoService): Defaults to Depends(get_photo_service)
+
+            Returns:
+                A list of photos using the PhotoRead schema
+        """
         try:
             all_photos = await photo_service.get_all_photos()
             return all_photos
@@ -27,7 +36,17 @@ class PhotoEndpoint:
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{e}')
         
-    async def fetch_photo_by_id(self, photo_id: int, photo_service: PhotoService = Depends(get_photo_service)):
+    async def fetch_photo_by_id(self, photo_id: int, photo_service: PhotoService = Depends(get_photo_service)) -> PhotoRead:
+        """
+            Fetches the photo by id.
+
+            Args:
+                photo_id (int): Gets the id of the photo
+                photo_service (PhotoService): Defaults to Depends(get_photo_service)
+
+            Returns:
+                A photo using the PhotoRead schema.
+        """
         try:
             photo = await photo_service.get_photo_by_id(photo_id=photo_id)
             if not photo:
@@ -39,6 +58,18 @@ class PhotoEndpoint:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{e}')
         
     async def fetch_limited_photos(self, skip: int = 0, limit: int = 10, photo_service: PhotoService = Depends(get_photo_service)):
+        """
+            Fetches a list of photos depending on the number of photos to
+            skip and the maximum number of photos to get
+
+            Args:
+                skip (int): The number of photos to skip
+                limit (int): Maximum number of photos to obtain
+                photo_service (PhotoService): Defaults to Depends(get_photo_service).
+
+            Returns:
+                A list of photos based on the PhotoRead schema.
+        """
         try:
             limited_photos = await photo_service.get_specific_photos(skip=skip, limit=limit)
             return limited_photos
--- app/routers/photos/photo_service.py
+++ app/routers/photos/photo_service.py
@@ -9,13 +9,29 @@ class PhotoService(IPhoto):
 
     @staticmethod
     async def get_all_photos() -> List[Photo]:
+        """
+            Gets all the photos
+
+            Returns:
+                A list of photos.
+        """
         photos = await Photo.find_all().to_list()
         return photos
 
     @staticmethod
     async def add_photos(photo_list: List[dict]) -> List[Photo]:
+        """
+            Inserts a list of photos to the database.
+
+            Args:
+                photo_list (List[dict]): Gets a list of photos
+
+            Returns:
+                photos based on the Photo Model
+        """
         photos = [Photo(**photo) for photo in photo_list]
         try:
+            # Insert list of Photo to the db
             await Photo.insert_many(photos)
             return photos
         except Exception as e:
@@ -23,11 +39,30 @@ class PhotoService(IPhoto):
     
     @staticmethod
     async def get_specific_photos(skip: int, limit: int) -> List[Photo]:
+        """
+            Get a limited list of photos
+
+            Args:
+                skip (int): The number of photos to skip
+                limit (int): Maximum number of photos to obtain
+            
+            Returns:
+                A list of photos based on the Photo Model
+        """
         limited_photos = await Photo.find(skip=skip, limit=limit).to_list()
         return limited_photos
     
     @staticmethod
     async def get_photo_by_id(photo_id: int) -> Photo:
+        """
+            Get photo by the id
+
+            Args:
+                photo_id (int): The id of the photo to get
+
+            Returns:
+                photo based on the Photo Model
+        """
         photo = await Photo.find_one(Photo.id == photo_id)
         if not photo:
             return None
