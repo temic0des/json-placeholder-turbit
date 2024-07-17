73ac0e22f01efbbaa697240e27bfb194d57bd3ee
Temi Rahman <Temi@Temi.fritz.box>
2024-07-17 14:51:58 +0200

    Comments added to the Comment feature

--- app/routers/comments/comment_endpoint.py
+++ app/routers/comments/comment_endpoint.py
@@ -19,6 +19,15 @@ class CommentEndpoint:
         self.comment_router.get('', response_model=List[CommentRead])(self.fetch_limited_comments)
 
     async def fetch_all_comments(self, comment_service: CommentService = Depends(get_comment_service)) -> List[CommentRead]:
+        """
+            Fetches all the comments
+
+            Args:
+                comment_service (CommentService): Defaults to Depends(get_comment_service)
+
+            Returns:
+                A list of comments using the CommentRead schema
+        """
         try:
             all_comments = await comment_service.get_all_comments()
             return all_comments
@@ -28,6 +37,16 @@ class CommentEndpoint:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{e}')
 
     async def fetch_comment_by_id(self, comment_id: int, comment_service: CommentService = Depends(get_comment_service)) -> CommentRead:
+        """
+            Fetches the comment by id.
+
+            Args:
+                comment_id (int): Gets the id of the comment
+                comment_service (CommentService): Defaults to Depends(get_comment_service)
+
+            Returns:
+                A comment using the CommentRead schema.
+        """
         try:
             comment = await comment_service.get_comment_by_id(comment_id=comment_id)
             return comment
@@ -37,6 +56,18 @@ class CommentEndpoint:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{e}')
         
     async def fetch_limited_comments(self, skip: int = 0, limit: int = 10, comment_service: CommentService = Depends(get_comment_service)):
+        """
+            Fetches a list of comments depending on the number of comments to
+            skip and the maximum number of comments to get
+
+            Args:
+                skip (int): The number of comments to skip
+                limit (int): Maximum number of comments to obtain
+                comment_service (CommentService): Defaults to Depends(get_comment_service).
+
+            Returns:
+                A list of comments based on the CommentRead schema.
+        """
         try:
             limited_comments = await comment_service.get_specific_comments(skip=skip, limit=limit)
             return limited_comments
--- app/routers/comments/comment_service.py
+++ app/routers/comments/comment_service.py
@@ -7,8 +7,18 @@ class CommentService(IComment):
 
     @staticmethod
     async def add_comments(comment_list: List[dict]) -> List[Comment]:
+        """
+            Inserts a list of comments to the database.
+
+            Args:
+                comment_list (List[dict]): Gets a list of commments
+
+            Returns:
+                Comments based on the Comment Schema
+        """
         comments = [Comment(**comment) for comment in comment_list]
         try:
+            # Insert many comments
             await Comment.insert_many(comments)
             return comments
         except Exception as e:
@@ -17,15 +27,34 @@ class CommentService(IComment):
     
     @staticmethod
     async def get_all_comments() -> List[Comment]:
+        """
+            Get all the comments
+
+            Returns:
+                A list of comments
+        """
         all_comments = await Comment.find_all().to_list()
         return all_comments
     
     @staticmethod
     async def get_specific_comments(skip: int, limit: int) -> List[Comment]:
+        """
+            Get a limited list of comments
+
+            Args:
+                skip (int): The number of comments to skip
+                limit (int): Maximum number of comments to obtain
+        """
         comments = await Comment.find(skip=skip, limit=limit).to_list()
         return comments
     
     @staticmethod
     async def get_comment_by_id(comment_id: int) -> Comment:
+        """
+            Get comment by the id
+
+            Args:
+                comment_id (int): The id of the comment to get
+        """
         comment = await Comment.find_one(Comment.id == comment_id)
         return comment
